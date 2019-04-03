276f3d327c95dc9d34922daab7d07667157f75be
Pavle Prica <pavle.prica5@gmail.com>
2019-04-03 19:31:13 +0200

    Update

--- kernel/console.c
+++ kernel/console.c
@@ -20,6 +20,9 @@
 #include <asm/io.h>
 #include <asm/system.h>
 #include <string.h>
+#include <unistd.h>
+#include <dirent.h>
+#include <fcntl.h>
 
 #define SCREEN_START 0xb8000
 #define SCREEN_END   0xc0000
@@ -423,7 +426,7 @@ void con_write(struct tty_struct * tty)
 					if(c == '#')
 					{
 					char oldC = c;
-					c = '$';
+					c = '~';
 					__asm__(
 						"movb attr, %%ah\n\t"
 						"movw %%ax, %1\n\t"
@@ -658,6 +661,45 @@ void tool_draw(void)
 		}
 	}
 	restore_cur();
+}
+
+void getdents_test(void)
+{
+	struct dirent entry; //struktura sa kojom radi getdents funkcija
+	int current_dir_fd;
+	int len;
+
+	//otvaramo trenutni direktorijum za citanje
+	current_dir_fd = open(".", O_RDONLY);
+
+	while (1)
+	{
+		//prvi argument je otvoreni fd za dir koji citamo
+
+		//drugi argument je struktura koja ce sadrzati rezultat
+
+		//treci argument bi trebalo da bude koliko stavki citamo,
+		//ali u Linux 0.0.1 se uvek cita jedna stavka i ovaj argument se ignorise
+
+		//ako hocemo da procitamo sve stavke u direktorijumu,
+		//moramo da pozovemo getdents vise puta nad istim fd
+
+		//povratna vrednost je duzina imena stavke ili 0 ako smo dosli do kraja
+		len = getdents(current_dir_fd, &entry, 1);
+
+		if (len > 0)
+		{
+			write(1, entry.d_name, len);
+			write(1, "\n", 1);
+		}
+		else
+		{
+			break;
+		}
+		
+	}
+
+	close(current_dir_fd);
 }
 
 void print_name_and_index(void)
